2f626f83e0d570b4e88e9c8a97d2a2ac329a6403
Denis Holdeew <h.denis@gmx.de>
2020-06-04 08:28:06 +0200

    Default interface methods - Add sample implementation

--- src/com/holidaydrills/interfaces/FormValidatorImpl.java
+++ src/com/holidaydrills/interfaces/FormValidatorImpl.java
@@ -2,6 +2,17 @@ package com.holidaydrills.interfaces;
 
 public class FormValidatorImpl implements Validator {
 
-
+    /*
+    The default implementation in the Validator interface defines the form field for additional information as invalid
+    if the input is longer than 200 characters.
+    In our implementation we want to be this kind of input field only valid if it is max 100 characters.
+    */
+    @Override
+    public boolean validateAdditionalInformation(String additionalInformation) {
+        if(additionalInformation.length() > 100) {
+            return false;
+        }
+        return true;
+    }
 
 }
--- src/com/holidaydrills/interfaces/Validator.java
+++ src/com/holidaydrills/interfaces/Validator.java
@@ -4,11 +4,19 @@ package com.holidaydrills.interfaces;
 interface Validator {
 
     /*
-    Email validation should be the same across all formatters implementing the Formatter interface.
+    Email validation should be the same across all validators implementing the Validator interface.
     The static interface method ensures, that implementing classes cannot override the method.
      */
     static boolean validateEmail(String email) {
         String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
         return email.matches(regex);
     }
+
+    default boolean validateAdditionalInformation(String additionalInformation) {
+        if(additionalInformation.length() > 200) {
+            return false;
+        }
+        return true;
+    }
+
 }
