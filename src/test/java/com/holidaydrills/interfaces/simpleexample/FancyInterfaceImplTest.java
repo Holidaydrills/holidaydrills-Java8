61405489b06258db2e7fe70371ca74986a634f8a
Denis Holdeew <h.denis@gmx.de>
2020-06-12 13:52:37 +0200

    Add tests for FancyInterfaceImpl

--- src/test/java/com/holidaydrills/interfaces/simpleexample/FancyInterfaceImplTest.java
+++ src/test/java/com/holidaydrills/interfaces/simpleexample/FancyInterfaceImplTest.java
@@ -4,9 +4,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FancyInterfaceImplTest {
+
+    FancyInterfaceImpl cut = new FancyInterfaceImpl();
+
+    @Test
+    public void test_default_method() {
+        assertEquals("Hi there", cut.sayHello());
+        assertEquals("Goodbye my friend!", cut.sayGoodBye());
+    }
+
+    @Test
+    public void test_default_method_overridden() {
+        assertEquals("Hello all! How are you?", cut.sayHelloAgain());
+    }
+
     @Test
     public void test_static_interface_method() {
-        FancyInterfaceImpl cut = new FancyInterfaceImpl();
         String expected = "This is a Fancy interface which provides methods to say hello and to say goodbye:)";
         assertEquals(expected, cut.getFancyInterfaceInfo());
         // Test directly on the interface
